21553b6b9997fae605473c23d8b34f9311190772
Bilal Khan <Bilal Khan@DESKTOP-RMG2TTN>
2022-01-22 17:22:06 +0500

    Add project files.

---
+++ DemoService/DemoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DemoService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "DemoService" in both code and config file together.
+    public class DemoService : IDemoService
+    {
+
+
+        public string GetName(string Name)
+        {
+            return $"Name: {Name}";
+        }
+    }
+}
